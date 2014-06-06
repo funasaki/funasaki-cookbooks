0bfc19cd9e812c3ba7bbf9802cfcfd52c9f42f7e
EC2 Default User <ec2-user@ip-10-0-1-40.ap-northeast-1.compute.internal>
2014-06-06 06:02:37 +0000

    update wp-cli default.rb

--- wp-cli/recipes/default.rb
+++ wp-cli/recipes/default.rb
@@ -5,7 +5,7 @@ package "mysql" do
 end
 
 execute "wp-cli install" do
-   command "curl -L https://raw.github.com/wp-cli/builds/gh-pages/phar/wp-cli.phar > wp-cli.phar && chmod +x wp-cli.phar && cp wp-cli.phar #{wpdir}/wp"
+   command "wget https://raw.github.com/wp-cli/builds/gh-pages/phar/wp-cli.phar -O wp-cli.phar && chmod +x wp-cli.phar && cp wp-cli.phar #{wpdir}/wp"
    cwd '/home/ec2-user'
    action :run
    not_if { ::File.exists?("#{wpdir}/wp")}
