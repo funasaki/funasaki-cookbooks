4ec6e7ebf126fb2b00021501acbf8fe168b31391
EC2 Default User <ec2-user@ip-10-160-33-250.ap-northeast-1.compute.internal>
2014-07-23 08:03:42 +0000

    second commit

--- hello/files/default/tests/minitest/default_test.rb
+++ hello/files/default/tests/minitest/default_test.rb
@@ -1,7 +1,7 @@
 require 'minitest/spec'
 describe_recipe 'hello::default' do
     it 'create hello.txt' do
-        file('/home/ec2-user/hello.txt').must_exist
+        file('/home/ec2-user/hello4.txt').must_exist
     end
 end
 
